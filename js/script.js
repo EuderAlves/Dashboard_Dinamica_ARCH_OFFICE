fd9271eaa836435d01d368b682bfc408c31701b2
Euder Alves <euder.alv@gmail.com>
2021-04-06 14:47:18 -0300

    Paginas criadas para dinamica

---
+++ js/script.js
@@ -0,0 +1,14 @@
+function clicou(){
+    alert("Você está saindo da página!");
+}
+
+function voltar(){
+    alert("Você voltou para a página inicial!");
+}
+function pag1(){
+    alert("Você vai ser direcionado a página de 'Manutenção de Usuários'");
+}
+
+function relatorio(){
+    alert("Seu relatório foi enviado com sucesso!");
+}
